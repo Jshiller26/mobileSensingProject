e590b164faa7c46fff9da2027674901643fd7c67
Jshiller26 <shiller1205@gmail.com>
2023-04-03 20:44:41 -0400

    Got permissions to record audio adn save audio working successfully.

--- app/src/main/java/com/example/mobilesensingproject/MainActivity.kt
+++ app/src/main/java/com/example/mobilesensingproject/MainActivity.kt
@@ -1,11 +1,45 @@
 package com.example.mobilesensingproject
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.core.app.ActivityCompat
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var recordButton: Button
+    lateinit var stopButton: Button
+    lateinit var playButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        recordButton = findViewById(R.id.button)
+        stopButton = findViewById(R.id.button2)
+        playButton = findViewById(R.id.button3)
+
+        // Initially disable buttons
+        recordButton.isEnabled = false
+        stopButton.isEnabled = false
+
+        // Request permission to record audio and save audio files
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED)
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.RECORD_AUDIO, Manifest.permission.WRITE_EXTERNAL_STORAGE), 111)
+        recordButton.isEnabled = true
     }
+
+    // If the user gives permission to record, enable recordButton
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode == 111 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+            recordButton.isEnabled = true
+    }
+
+
 }
