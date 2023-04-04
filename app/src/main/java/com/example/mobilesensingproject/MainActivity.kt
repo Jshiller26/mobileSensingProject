184770fa3807c5f01f9e7f89cd31428dd0c15fae
Jshiller26 <shiller1205@gmail.com>
2023-04-03 21:01:54 -0400

    Added functions to start and stop recording.  Also declared and set up path to save audio to.

--- app/src/main/java/com/example/mobilesensingproject/MainActivity.kt
+++ app/src/main/java/com/example/mobilesensingproject/MainActivity.kt
@@ -2,8 +2,10 @@ package com.example.mobilesensingproject
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.media.MediaRecorder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
 import android.widget.Button
 import androidx.core.app.ActivityCompat
 
@@ -12,9 +14,14 @@ class MainActivity : AppCompatActivity() {
     lateinit var recordButton: Button
     lateinit var stopButton: Button
     lateinit var playButton: Button
+    lateinit var mr: MediaRecorder
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Audio recordings will get saved to /genreGuesser
+        var storagePath = Environment.getExternalStorageDirectory().toString()+"/genreGuesser.mp3"
+        mr = MediaRecorder()
 
         recordButton = findViewById(R.id.button)
         stopButton = findViewById(R.id.button2)
@@ -28,6 +35,28 @@ class MainActivity : AppCompatActivity() {
         if(ActivityCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED)
             ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.RECORD_AUDIO, Manifest.permission.WRITE_EXTERNAL_STORAGE), 111)
         recordButton.isEnabled = true
+
+        // Record audio when record button is clicked
+        recordButton.setOnClickListener{
+            mr.setAudioSource(MediaRecorder.AudioSource.MIC)
+            // Save audio in mp3 format to "storagePath"
+            mr.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4)
+            mr.setAudioEncoder(MediaRecorder.AudioEncoder.AAC)
+            mr.setAudioEncodingBitRate(16 * 44100)
+            mr.setAudioSamplingRate(44100)
+            mr.setOutputFile(storagePath)
+            // Start recording and enable stop button
+            mr.prepare()
+            mr.start()
+            stopButton.isEnabled = true
+            recordButton.isEnabled = false
+        }
+
+        // Stop recording when stop button is clicked
+        stopButton.setOnClickListener{
+            mr.stop()
+            recordButton.isEnabled = true
+        }
     }
 
     // If the user gives permission to record, enable recordButton
